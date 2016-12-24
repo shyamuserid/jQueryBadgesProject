543040b2fa18fe307082c564c0ef16f1e4175eaa
SHYAM NAMAVARAM <shyamuserid@gmail.com>
2016-12-24 00:44:15 -0500

    Solved Project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,41 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/shyamuserid.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+    }
+  });
+
+  function addCourses(courses){
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      console.log(course);
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course Contents'
+      }).appendTo($course);
+
+    })
+  }
 
 });
